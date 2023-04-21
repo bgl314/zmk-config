a6e16e105c6411eada649c4e637f4594cf10b60d
bgl314 <bgl314@yahoo.com>
2023-04-21 11:11:05 -0400

    Initial User Config.

---
+++ config/corne/build.ps1
@@ -0,0 +1,3 @@
+cd C:/Users/bgl31/Documents/GitHub/zmk/app
+west build -p -d ./build/right  -b nice_nano_v2 -- -DZMK_CONFIG="C:/Users/bgl31/Documents/GitHub/zmk-config/config/corne" -DSHIELD="corne_right" -Wno-dev
+west build -p -d ./build/left  -b nice_nano_v2 -- -DZMK_CONFIG="C:/Users/bgl31/Documents/GitHub/zmk-config/config/corne" -DSHIELD="corne_left nice_view_adapter nice_view" -Wno-dev
