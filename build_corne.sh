b47f8d20acd8f18bcd5d4ac22004111a968fdd97
bgl314 <bgl314@yahoo.com>
2024-05-27 17:12:19 -0400

    added

---
+++ build_corne.sh
@@ -0,0 +1,4 @@
+cd ../zmk/app
+west build -p -d ../build/right  -b nice_nano_v2 -- -DZMK_CONFIG="/mnt/c/Users/bgl31/Documents/GitHub/zmk-config/config" -DSHIELD="corne_right nice_view_adapter nice_view" -Wno-dev
+west build -p -d ../build/left  -b nice_nano_v2 -- -DZMK_CONFIG="/mnt/c/Users/bgl31/Documents/GitHub/zmk-config/config" -DSHIELD="corne_left nice_view_adapter nice_view" -Wno-dev
+
--- config/corne/build.ps1
+++
@@ -1,3 +0,0 @@
-cd C:/Users/bgl31/Documents/GitHub/zmk/app
-west build -p -d ../build/right  -b nice_nano_v2 -- -DZMK_CONFIG="C:/Users/bgl31/Documents/GitHub/zmk-config/config" -DSHIELD="corne_right" -Wno-dev
-west build -p -d ../build/left  -b nice_nano_v2 -- -DZMK_CONFIG="C:/Users/bgl31/Documents/GitHub/zmk-config/config" -DSHIELD="corne_left nice_view_adapter nice_view" -Wno-dev
